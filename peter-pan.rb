365b95d59019b38d94be07611d9fe565104d4680
Matthew Nielsen <xunker@pyxidis.org>
2014-02-02 22:08:50 -0700

    removed redundant code

--- peter-pan.rb
+++ peter-pan.rb
@@ -71,148 +71,62 @@ class PeterPan
   end
 
   def calculate_integral_points(x1, y1, x2, y2)
-    # pt1 = [x1, y1]
-    # pt2 = [x2, y2]
-    # m = (pt1[1] - pt2[1]) / (pt1[0] - pt2[0])
-    # b = pt1[1] - m * pt1[0]
+    x_integrals = calculate_integrals(x1, x2)
+    y_integrals = calculate_integrals(y1, y2)
 
-    # i = pt1[0]
-    # points = []
-    # while i <= pt2[0] do
-    #   points << [i, m * i + b]
-    #   i += 1
-    # end
-    # points
-
-    x_integrals = []
-    if x1 < x2
-      x1.upto(x2) do |i|
-        x_integrals << i
-      end
-    else
-      x1.downto(x2) do |i|
-        x_integrals << i
-      end
-    end
-
-    y_integrals = []
-    if y1 < y2
-      y1.upto(y2) do |i|
-        y_integrals << i
-      end
-    else
-      y1.downto(y2) do |i|
-        y_integrals << i
-      end
-    end
-
-    while x_integrals.length < y_integrals.length
-      x_integrals.unshift(x_integrals.first)
-    end
-
-    while y_integrals.length < x_integrals.length
-      y_integrals.unshift(y_integrals.first)
-    end
+    (x_integrals, y_integrals) = standardize_integral_lengths(x_integrals, y_integrals)
 
     x_integrals.zip(y_integrals)
-    # [ x_integrals, y_integrals ]
+  end
+
+  def standardize_integral_lengths(one, two)
+    while one.length < two.length
+      one.unshift(one.first)
+    end
+
+    while two.length < one.length
+      two.unshift(two.first)
+    end
+
+    [one, two]
+  end
+
+  def calculate_integrals(starting, ending)
+    integrals = []
+    if starting < ending
+      starting.upto(ending) do |i|
+        integrals << i
+      end
+    else
+      starting.downto(ending) do |i|
+        integrals << i
+      end
+    end
+    integrals
   end
 end
 
 p = PeterPan.new
-# [0,7,14,21,28,35,42].each do |i|
-# 0.step(100, 7) do |i|
-#   25.times do |ii|
-#     p.plot(i+ii,ii, i)
-#   end
-# end
-# p.plot(0,6, 'A')
-# p.plot(2,6, 'B')
-# p.plot(4,6, 'C')
-# p.plot(35,6, 'D')
-# p.plot(5,6, 'F')
-# p.plot(7,6, 'G')
-# p.plot(9,6, 'H')
-# p.plot(30,4, 'I')
-
-# puts p.show_buffer
-
-# # 42.times do |i|
-# #   puts p.show_viewport(i,0)
-# #   sleep(0.1)
-# # end
-# # 15.times do |i|
-# #   puts p.show_viewport(0,i)
-# #   sleep(0.1)
-# # end
-
-# pt1 = [0, 0]
-# pt2 = [10, 10]
-# m = (pt1[1] - pt2[1]) / (pt1[0] - pt2[0])
-# b = pt1[1] - m * pt1[0]
-
-# i = pt1[0]
-# points = []
-# while i <= pt2[0] do
-#   points << [i, m * i + b]
-#   i += 1
-# end
-
-# # puts points.inspect
-# points.each do |px, py|
-#   puts p.show_viewport(px, py)
-#   sleep(0.1)
-# end
-
-###
-
-# font = YAML.load(File.new('./fonts/transpo.yml').read)
-# p.write(font, 0, 0, 'Hello, world!')
-# puts p.show_buffer
-
-# pt1 = [0, 0]
-# pt2 = [p.buffer_width-1, 0]
-# m = (pt1[1] - pt2[1]) / (pt1[0] - pt2[0])
-# b = pt1[1] - m * pt1[0]
-
-# i = pt1[0]
-# points = []
-# while i <= pt2[0] do
-#   points << [i, m * i + b]
-#   i += 1
-# end
-
-# # puts points.inspect
-# points.each do |px, py|
-#   puts p.show_viewport(px, py)
-#   sleep(0.05)
-# end
-
-###
 
 font = YAML.load(File.new('./fonts/transpo.yml').read)
 p.write(font, 0, 0, 'Hello,')
 p.write(font, 0, font["height"]+1, 'world!')
 puts p.show_buffer
 
+start_x=0
+start_y=0
 loop do
-  p.calculate_integral_points(0,0,p.buffer_width-p.viewport_width,0).each do |px, py|
-    puts p.show_viewport(px, py)
-    sleep(0.05)
-  end
-
-  p.calculate_integral_points(p.buffer_width-p.viewport_width,0,0,font["height"]+1).each do |px, py|
-    puts p.show_viewport(px, py)
-    sleep(0.05)
-  end
-
-  p.calculate_integral_points(0, font["height"]+1,p.buffer_width-p.viewport_width, font["height"]+1).each do |px, py|
-    puts p.show_viewport(px, py)
-    sleep(0.05)
-  end
-
-  p.calculate_integral_points(p.buffer_width-p.viewport_width, font["height"]+1, 0, 0).each do |px, py|
-    puts p.show_viewport(px, py)
-    sleep(0.05)
+  [
+    [p.buffer_width-p.viewport_width,0],
+    [0,font["height"]+1],
+    [p.buffer_width-p.viewport_width, font["height"]+1],
+    [0, 0]
+  ].each do |x,y|
+    p.calculate_integral_points(start_x, start_y, x, y).each do |px, py|
+      puts p.show_viewport(px, py)
+      sleep(0.1)
+    end
+    start_x = x
+    start_y = y
   end
 end
