a93d1d8dca5565b10b19f800611a1a2aa4aba8e1
Matthew Nielsen <xunker@pyxidis.org>
2014-02-02 19:16:52 -0700

    panning viewport over buffer

--- peter-pan.rb
+++ peter-pan.rb
@@ -44,23 +44,46 @@ class PeterPan
 end
 
 p = PeterPan.new
-[0,7,14,21,28,35,42].each do |i|
-  10.times do |ii|
-    p.plot(i+ii,ii, i)
-  end
-end
-# p.plot(0,6, 'A')
-# p.plot(2,6, 'B')
-# p.plot(4,6, 'C')
-# p.plot(35,6, 'D')
+# [0,7,14,21,28,35,42].each do |i|
+# 0.step(100, 7) do |i|
+#   25.times do |ii|
+#     p.plot(i+ii,ii, i)
+#   end
+# end
+p.plot(0,6, 'A')
+p.plot(2,6, 'B')
+p.plot(4,6, 'C')
+p.plot(35,6, 'D')
+p.plot(5,6, 'F')
+p.plot(7,6, 'G')
+p.plot(9,6, 'H')
+p.plot(30,4, 'I')
 
 puts p.show_buffer
 
-42.times do |i|
-  puts p.show_viewport(i,0)
-  sleep(0.1)
+# 42.times do |i|
+#   puts p.show_viewport(i,0)
+#   sleep(0.1)
+# end
+# 15.times do |i|
+#   puts p.show_viewport(0,i)
+#   sleep(0.1)
+# end
+
+pt1 = [0, 0]
+pt2 = [10, 10]
+m = (pt1[1] - pt2[1]) / (pt1[0] - pt2[0])
+b = pt1[1] - m * pt1[0]
+
+i = pt1[0]
+points = []
+while i <= pt2[0] do
+  points << [i, m * i + b]
+  i += 1
 end
-15.times do |i|
-  puts p.show_viewport(0,i)
+
+# puts points.inspect
+points.each do |px, py|
+  puts p.show_viewport(px, py)
   sleep(0.1)
 end
